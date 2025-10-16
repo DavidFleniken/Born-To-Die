cb79ddac0ac5931e612b6d60403c8e3553061ce7
Eggy <davidfleniken443@gmail.com>
2025-10-15 23:29:18 -0400

    Walking Anim for Xana

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,15 +12,29 @@ public interface IInteractable
 }
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour, IInteractor
 {
+    //There should only be one "player"
+    private static PlayerController singleton;
+
     [SerializeField] float speed = 5f;
     Rigidbody2D rb;
+    Animator anim;
     Vector2 velo;
 
     private void Start()
     {
+        //check theres only one player
+        if (singleton != null)
+        {
+            Debug.LogError("Multiple Player Controller Scripts detected!");
+            return;
+        }
+        singleton = this;
+
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -36,6 +50,21 @@ public class PlayerController : MonoBehaviour, IInteractor
 
         #endregion
 
+
+        // anim data
+        bool isMoving = false;
+        if (velo.magnitude > 0.01f)
+        {
+            isMoving = true;
+        }
+
+        anim.SetFloat("Y Velo", velo.y);
+
+        // Give up-down animation more priority then left-right
+        if (Mathf.Abs(velo.y) > 0) anim.SetFloat("X Velo", 0);
+        else anim.SetFloat("X Velo", velo.x);
+
+        anim.SetBool("isMoving", isMoving);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
