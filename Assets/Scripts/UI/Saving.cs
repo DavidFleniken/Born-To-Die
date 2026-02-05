feb55d3808f6d17e4c60df50f08e2653bf7bd0f4
metinis <denis.muraska@gmail.com>
2026-02-04 19:01:48 -0500

    save image

--- Assets/Scripts/UI/Saving.cs
+++ Assets/Scripts/UI/Saving.cs
@@ -1,10 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using YamlDotNet.Serialization;
+using Object = UnityEngine.Object;
 
 public class Saving : MonoBehaviour
 {
@@ -21,11 +25,20 @@ public class Saving : MonoBehaviour
         public Dictionary<string, bool> act1Flags;
     }
 
+    [SerializeField] private Button saveButton;
     [SerializeField] private Settings settings;
+    [SerializeField] private Camera cam;
+    [SerializeField] private int index = 1;
+
+    private void OnEnable()
+    {
+        LoadSaveImage("screenshot" + index + ".png");
+    }
 
     public void Save()
     {
         Debug.Log("Saving...");
+        
         
         Vector3 pos = GameObject.FindGameObjectWithTag("Player").transform.position;
         
@@ -50,6 +63,30 @@ public class Saving : MonoBehaviour
         File.WriteAllText(path, yaml);
 
         Debug.Log($"Saved to {path}");
+        settings.toggleSettings();
+        TakeScreenshot();
+    }
+
+    public void LoadSaveImage(string name)
+    {
+        string path = Path.Combine(Application.persistentDataPath, name);
+    
+        if (File.Exists(path))
+        {
+            byte[] fileData = File.ReadAllBytes(path);
+            Texture2D texture = new Texture2D(2, 2);
+            texture.LoadImage(fileData); // Auto-resizes texture
+        
+            Sprite sprite = Sprite.Create(texture, 
+                new Rect(0, 0, texture.width, texture.height), 
+                new Vector2(0.5f, 0.5f));
+        
+            saveButton.image.sprite = sprite;
+        }
+        else
+        {
+            Debug.LogError($"File not found: {path}");
+        }
     }
     public void Load()
     {
@@ -115,5 +152,32 @@ public class Saving : MonoBehaviour
         PlayerController.lastMode = data.movementMode;
         PlayerController.curMode = data.movementMode;
         Act1GameManager.trackedInteractions = new Dictionary<string, bool>(data.act1Flags);
+    }
+    void SaveRenderTexture(RenderTexture rt, string path)
+    {
+        RenderTexture.active = rt;
+        Texture2D tex = new Texture2D(rt.width, rt.height, TextureFormat.RGB24, false);
+        tex.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
+        RenderTexture.active = null;
+        var bytes = tex.EncodeToPNG();
+        System.IO.File.WriteAllBytes(path, bytes);
+        AssetDatabase.ImportAsset(path);
+        Debug.Log($"Saved texture: {rt.width}x{rt.height} - " + path);
+    }
+    [MenuItem("Assets/Take Screenshot", true)]
+    public bool TakeScreenshotValidation() =>
+        Selection.activeGameObject && Selection.activeGameObject.GetComponent<Camera>();
+    [MenuItem("Assets/Take Screenshot")]
+    public void TakeScreenshot()
+    {
+        var prev = cam.targetTexture;
+        var rt = new RenderTexture(Screen.width, Screen.height, 16);
+        cam.targetTexture = rt;
+        cam.Render();
+        //todo use index
+        string path = Path.Combine(Application.persistentDataPath, "screenshot" + index + ".png");
+        SaveRenderTexture(rt, path);
+        cam.targetTexture = prev;
+        Object.DestroyImmediate(rt);
     }
 }
